ae7b51aa36595eeafab1001d7f19d67e362bb96d
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2024-04-11 16:14:37 +0900

    training quality changes updated

--- examples/train_video.py
+++ examples/train_video.py
@@ -396,6 +396,7 @@ def main(argv):
         split="train",
         transform=train_transforms,
     )
+    train_dataset.max_frames = 8
     test_dataset = VideoFolder(
         args.dataset,
         rnd_interval=False,
@@ -403,7 +404,8 @@ def main(argv):
         split="test",
         transform=test_transforms,
     )
-
+    test_dataset.max_frames = 8
+    print('max frames :', train_dataset.max_frames)
     device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
     train_dataloader = DataLoader(
